1a510d50535cab74ffdc5c273c44e1ee70abc8e0
Timothy Lim <timlimytn@gmail.com>
2024-12-18 17:17:22 +0800

    Updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ export default {
   apps: [
     {
       name: "nexus-application",
-      script: "server.js",
+      script: "/root/.nvm/versions/node/v23.4.0/bin/npm",
+      args: "run start",
       env: {
         NODE_ENV: "development",
         ENV_VAR1: "environment_variable",
